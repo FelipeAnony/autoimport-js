aa7dde6817fe4ab08a228bdebfc2ffc047c31feb
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-04-10 21:11:58 +0200

    feat: ensures autoimport calls FsHelper with correct value

--- src/__tests__/autoimport.test.ts
+++ src/__tests__/autoimport.test.ts
@@ -2,15 +2,25 @@ import path from 'path';
 
 import { autoimport } from '../autoimport';
 import { BadPathError, MissingParamError } from '../errors';
+import { FsHelper } from "../helpers/fs-helper";
 
 describe('Autoimport', () => {
-    it('Should throws MissingParam error if theres no folder path provided', () => {
-        expect(autoimport).toThrow(new MissingParamError('folderPath'));
+    it('Should throws MissingParam error if theres no folder path provided', async () => {
+        await expect(autoimport).rejects.toThrow(new MissingParamError('folderPath'));
     });
 
-    it('Should throws a BadPath error if an invalid folder path is provided', () => {
-        expect(() => autoimport('1nvalid folder path')).toThrow(
+    it('Should throws a BadPath error if an invalid folder path is provided', async () => {
+        await expect(() => autoimport('1nvalid folder path')).rejects.toThrow(
             new BadPathError(),
         );
+    });
+
+    it("Should calls FsHelper with correct value", async () => {
+        const getAllFilesPathSpy = jest.spyOn(FsHelper, "getAllFilesPathFromFolder");
+        getAllFilesPathSpy.mockImplementationOnce((path: string) => Promise.resolve([]));
+
+        await autoimport("/valid/folder");
+
+        expect(getAllFilesPathSpy).toBeCalledWith("/valid/folder");
     });
 });
--- src/autoimport.ts
+++ src/autoimport.ts
@@ -1,8 +1,11 @@
 import { BadPathError, MissingParamError } from './errors';
+import { FsHelper } from './helpers/fs-helper';
 
 const folderPathRegExp = /^(\/[a-zA-Z0-9_\-]+)+\/?$/;
 
-export const autoimport = (folderPath: string) => {
+export const autoimport = async (folderPath: string) => {
     if (!folderPath) throw new MissingParamError('folderPath');
     if (!folderPathRegExp.test(folderPath)) throw new BadPathError();
+
+    const filesList = await FsHelper.getAllFilesPathFromFolder(folderPath);
 };

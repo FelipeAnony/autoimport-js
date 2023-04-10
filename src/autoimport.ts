b2781061176d51c3643a5662be1ce34739abf3dc
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-04-10 21:55:49 +0200

    docs: adds JsDoc annotations to autoimport

--- src/autoimport.ts
+++ src/autoimport.ts
@@ -3,6 +3,14 @@ import { FsHelper } from './helpers/fs-helper';
 
 const folderPathRegExp = /^(\/[a-zA-Z0-9_\-]+)+\/?$/;
 
+/**
+ * Retrieves and returns dynamically imported files from a folder.
+ *
+ * @param {string} folderPath - The absolute path of the folder to retrieve files from.
+ * @throws {MissingParamError} - Throws if `folderPath` is not provided.
+ * @throws {BadPathError} - Throws if `folderPath` is not a valid path.
+ * @returns {Promise<Array<*>>} - Returns a promise that resolves to an array of dynamically imported files.
+ */
 export const autoimport = async (folderPath: string) => {
     if (!folderPath) throw new MissingParamError('folderPath');
     if (!folderPathRegExp.test(folderPath)) throw new BadPathError();
